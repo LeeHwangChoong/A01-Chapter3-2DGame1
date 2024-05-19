825487d85d04d2325539052cd5c460d6ea5cb03c
mwomwo1 <amongll312@gmail.com>
2024-05-19 13:22:46 +0900

    UI
    -Add life active
    -Add Score active

--- Assets/Scripts/Enemy/EnemyData.cs
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class EnemyData // : MonoBehaviour
 {
-    //적 데이터
-    public int Hp { get; set; } // 몇 번 피격하면 파괴되는지 1~3
-    public float Speed { get; set; } // 이동 속도
-
+    public int Hp { get; set; }
+    public float Speed { get; set; }
 
     public EnemyData(int hp, float speed)
     {
--- Assets/Scripts/Enemy/NormalEnemy.cs
+++ Assets/Scripts/Enemy/NormalEnemy.cs
@@ -5,6 +5,9 @@ public class NormalEnemy : MonoBehaviour
 {
     public GameObject normalEnemy;
     public EnemyData enemy = new(1, 0.01f);
+    
+    public GameObject player;
+    public int enemyScore;
 
     void Start()
     {
@@ -16,11 +19,9 @@ public class NormalEnemy : MonoBehaviour
         MoveEnemy(enemy);
     }
 
-    // 일반 적 생성
-
     private void SpawnEnemy()
     {
-        Debug.Log("화면 상단에 적 랜덤 생성");
+        Debug.Log("SpawnEnemy");
 
         float x = Random.Range(-2.5f, 2.5f);
         float y = 5.0f;
@@ -29,14 +30,12 @@ public class NormalEnemy : MonoBehaviour
 
     private void MoveEnemy(EnemyData enemy)
     {
-        // 살아있는 적 기본 이동(하강)
-
         if (enemy.Hp > 0)
         {
             transform.position += Vector3.down * enemy.Speed;
             if (transform.position.y < -5.0f)
             {
-                Debug.Log("화면 최하단에 도달한 적 오브젝트를 파괴합니다.");
+                Debug.Log("EnemyMove");
                 Destroy(normalEnemy, 3.0f);
             }
         }
@@ -44,18 +43,14 @@ public class NormalEnemy : MonoBehaviour
 
     private void Attack()
     {
-        // 총알 생성 함수 호출하기
+
     }
 
-    //이하 충돌 감지
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            // 살아 있을 때 player의 bullet에 맞으면
-            // Hp가 줄고 player의 bullet을 파괴한다
-
             if (enemy.Hp > 0)
             {
                 enemy.Hp -= 1;
@@ -63,14 +58,11 @@ public class NormalEnemy : MonoBehaviour
             }
             else if (enemy.Hp == 0)
             {
+                //Get Score
+                Player playerLogic = player.GetComponent<Player>();
+                playerLogic.score += enemyScore;
                 Destroy(normalEnemy, 3.0f);
-                //GameManager.Instance.AddScore();
             }
-
-            // TODO: 플레이어와 충돌 시에도 피해 줌
-
-            // TODO: (HP가 1 이상인 적만) HP가 1 남았을때 붉은 색조 입히기
-
         }
     }
 }
