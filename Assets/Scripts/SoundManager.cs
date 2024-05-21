aaf2fda854180567981d2c865eabbb2eff002b32
LeeHwangChoong <lyd8224@gmail.com>
2024-05-21 16:07:56 +0900

    Item Sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    private AudioSource audioSource;
+
+    public float volume = 1f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.volume = volume;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetVolume(float newVolume)
+    {
+        volume = Mathf.Clamp(newVolume, 0f, 1f); // 볼륨 값 제한
+        audioSource.volume = volume; // 오디오 소스 볼륨 업데이트
+    }
+
+    public void ItemSound(AudioClip itemSoundClip)
+    {
+        if (itemSoundClip != null)
+        {
+            audioSource.PlayOneShot(itemSoundClip);
+        }
+    }
+}
