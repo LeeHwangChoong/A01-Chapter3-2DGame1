64af2891c2595df62fd8233925ecb220ccfc37d7
LeeHwangChoong <lyd8224@gmail.com>
2024-05-21 16:43:42 +0900

    init

--- Assets/Scripts/Item/Shield.cs
+++ Assets/Scripts/Item/Shield.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 
 public class Shield : MonoBehaviour
-{
+{        
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
         {
             Destroy(other.gameObject);
         }
-    }
+    }    
+
 }
