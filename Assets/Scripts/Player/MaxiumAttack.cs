c16469f28aac9330f2b3439d06ce48ad0a765734
junhee0226 <jun4423577@naver.com>
2024-05-22 19:30:08 +0900

    LJH_sujung
    수정

--- Assets/Scripts/Player/MaxiumAttack.cs
+++ Assets/Scripts/Player/MaxiumAttack.cs
@@ -7,12 +7,14 @@ public class MaxiumAttack : MonoBehaviour
 {
     public GameObject Enemy1;
     public GameObject Enemy2;
+
     // Start is called before the first frame update
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            SoundManager.Instance.PlayerDeadSound();
             Destroy(collision.gameObject);
         }
     }
